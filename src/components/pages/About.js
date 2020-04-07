108e09eb5d11269aadac9b8a16697c382d1ce3c8
clembabs <clembabs10@gmail.com>
2020-04-07 12:38:02 +0100

    React Git

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+
+export const About = () => {
+    return (
+        <Fragment>
+        
+            <h1>About this App</h1>
+            <p>An App created to search Github users </p>
+        </Fragment>
+    )
+}
+
+export default About;
